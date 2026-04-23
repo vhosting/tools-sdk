1ae8f011ed320a021e3b62c7097893605f605299
Luca Patera <lucapatera@outlook.it>
2026-04-23 16:02:15 +0200

    feat: add plans method to ProxmoxResource and update ToolsSdk facade

--- src/Facades/ToolsSdk.php
+++ src/Facades/ToolsSdk.php
@@ -4,10 +4,12 @@ namespace VHosting\ToolsSdk\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Saloon\Laravel\Facades\Saloon;
+use VHosting\ToolsSdk\Resources\ProxmoxResource;
 use VHosting\ToolsSdk\Resources\WorkflowResource;
 
 /**
  * @method static WorkflowResource workflow()
+ * @method static ProxmoxResource proxmox()
  * @see \VHosting\ToolsSdk\ToolsConnector
  */
 class ToolsSdk extends Facade
--- src/Resources/ProxmoxResource.php
+++ src/Resources/ProxmoxResource.php
@@ -3,11 +3,17 @@
 namespace VHosting\ToolsSdk\Resources;
 
 use Saloon\Http\BaseResource;
+use VHosting\ToolsSdk\Requests\Proxmox\GetPlans;
 use VHosting\ToolsSdk\Requests\Proxmox\GetVm;
 use VHosting\ToolsSdk\Types\ProxmoxVm;
 
 class ProxmoxResource extends BaseResource
 {
+    public function plans(): array
+    {
+        return $this->connector->send(new GetPlans())->dto();
+    }
+    
     public function getVm(int $id): ProxmoxVm
     {
         return $this->connector->send(new GetVm($id))->dto();
